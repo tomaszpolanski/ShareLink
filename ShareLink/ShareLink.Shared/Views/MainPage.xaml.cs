a19f95566d449e1ef44df2e5f120172e14c389a8
tomaszpolanski <krigerek@gmail.com>
2014-10-13 18:28:53 +0200

    Improving text selection after taking application into foreground

--- ShareLink/ShareLink.Shared/Views/MainPage.xaml.cs
+++ ShareLink/ShareLink.Shared/Views/MainPage.xaml.cs
@@ -7,44 +7,6 @@
             InitializeComponent();
         }
 
-        //private void Current_VisibilityChanged(object sender, VisibilityChangedEventArgs e)
-        //{
-        //    if (e.Visible)
-        //    {
-        //        GetCopiedText();
-        //        LinkBox.Focus(FocusState.Programmatic);
-        //        LinkBox.SelectAll();
-        //    }
-        //}
-
-        //private async void GetCopiedText()
-        //{
-        //    try
-        //    {
-        //        var text = await Clipboard.GetContent().GetTextAsync();
-        //        if (!string.IsNullOrEmpty(text))
-        //        {
-        //            LinkBox.Text = text;
-        //            ShareButton.IsEnabled = Uri.IsWellFormedUriString(FormattedString, UriKind.Absolute);
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //    }
-        //}
-
-
-        //private void ShareTextHandler(DataTransferManager sender, DataRequestedEventArgs e)
-        //{
-        //    DataRequest request = e.Request;
-
-        //    var uri = new Uri(FormattedString);
-        //    request.Data.Properties.Title = "Share link";
-        //    request.Data.Properties.Description = uri.Host;
-
-        //    request.Data.SetWebLink(uri);
-        //}
-
 
         //private void LinkBox_OnTextChanged(object sender, TextChangedEventArgs e)
         //{
--- ViewModels/ViewModels/MainPageViewModel.cs
+++ ViewModels/ViewModels/MainPageViewModel.cs
@@ -12,8 +12,7 @@ namespace ShareLink.ViewModels.ViewModels
         private readonly ReadonlyReactiveProperty<string> _formattedString;
         private readonly ReadonlyReactiveProperty<bool> _isValid;
 
-        private readonly IDisposable _updateSubscription;
-        private readonly IDisposable _clipboardTextSubscription;
+        private readonly IDisposable _updateButtonSubscription;
         public ReadonlyReactiveProperty<bool> UpdateProperty { get; private set; }
 
         public ReactiveProperty<string> Text { get; private set; }
@@ -22,41 +21,44 @@ namespace ShareLink.ViewModels.ViewModels
         public MainPageViewModel(IWindowService windowService, IDataTransferService dataTransferService, IClipboardService clipboardService)
         {
             var isVisibleObservable = windowService.IsVisible;
-            Text = new ReactiveProperty<string>(string.Empty);
-            _formattedString = Text.Select(AddPrefixIfNeeded).ToReadonlyReactiveProperty();
+
+            var clipboardChangedObservable = isVisibleObservable.Select(
+               isVisible => isVisible ? Observable.FromAsync(clipboardService.GetTextAsync) : Observable.Empty<string>())
+                                                          .Switch()
+                                                          .Where(clipboardText => !string.IsNullOrEmpty(clipboardText));
+
+            Text = clipboardChangedObservable.ToReactiveProperty();
+
+            UpdateProperty = clipboardChangedObservable.Delay(TimeSpan.FromMilliseconds(300))
+                     .Select(_ => UpdateProperty != null && !UpdateProperty.Value)
+                     .ToReadonlyReactiveProperty();
+
+            _formattedString = Text.WhereIsNotNull()
+                                   .Select(AddPrefixIfNeeded)
+                                   .ToReadonlyReactiveProperty();
 
             _isValid = _formattedString.Select(text => Uri.IsWellFormedUriString(text, UriKind.Absolute))
-                .DistinctUntilChanged()
-                .ToReadonlyReactiveProperty();
+                                       .DistinctUntilChanged()
+                                       .ToReadonlyReactiveProperty();
 
-            var clipboardTextSubscription = isVisibleObservable.Select(
-                    isVisible =>
-                        isVisible ? Observable.FromAsync(clipboardService.GetTextAsync) : Observable.Empty<string>())
-                .Switch()
-                .Where(clipboardText => !string.IsNullOrEmpty(clipboardText));
-            UpdateProperty =
-                Text.Sample(isVisibleObservable.Select(_ => string.Empty))
-                .Select(_ => UpdateProperty != null && !UpdateProperty.Value)
-                    .ToReadonlyReactiveProperty();
-            _clipboardTextSubscription = clipboardTextSubscription
-                .Subscribe(text => Text.Value = text);
+
+
 
             ShareCommand =
                 new DelegateCommand(
-                    () =>
-                        dataTransferService.Share("Share link", _formattedString.Value, new Uri(_formattedString.Value)),
+                    () => dataTransferService.Share("Share link", _formattedString.Value, new Uri(_formattedString.Value)),
                     () => _isValid.Value);
 
-            _updateSubscription = _isValid.Subscribe(_ => ShareCommand.RaiseCanExecuteChanged());
+            _updateButtonSubscription = _isValid.Subscribe(_ => ShareCommand.RaiseCanExecuteChanged());
         }
 
         public void Dispose()
         {
-            _updateSubscription.Dispose();
+            _updateButtonSubscription.Dispose();
             _formattedString.Dispose();
             _isValid.Dispose();
-            _clipboardTextSubscription.Dispose();
             Text.Dispose();
+            UpdateProperty.Dispose();
         }
 
         private static string AddPrefixIfNeeded(string text)
