af911223f7c1e28d78fca677a096e5bec6304399
tomaszpolanski <krigerek@gmail.com>
2014-10-14 19:29:52 +0200

    Cleanup

--- ShareLink/ShareLink.Shared/Views/MainPage.xaml.cs
+++ ShareLink/ShareLink.Shared/Views/MainPage.xaml.cs
@@ -6,20 +6,5 @@
         {
             InitializeComponent();
         }
-
-
-        //private void LinkBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        //{
-        //    ShareButton.IsEnabled = Uri.IsWellFormedUriString(FormattedString, UriKind.Absolute);
-        //}
-
-        //private void LinkBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
-        //{
-        //    if (e.Key == VirtualKey.Enter)
-        //    {
-        //        DataTransferManager.ShowShareUI();
-        //        e.Handled = true;
-        //    }
-        //}
     }
 }
--- ViewModels/ViewModels/MainPageViewModel.cs
+++ ViewModels/ViewModels/MainPageViewModel.cs
@@ -22,10 +22,11 @@ namespace ShareLink.ViewModels.ViewModels
 
         public MainPageViewModel(IWindowService windowService, IDataTransferService dataTransferService, IClipboardService clipboardService)
         {
-            var clipboardChangedObservable = windowService.IsVisible.Select(isVisible => isVisible ? Observable.FromAsync(clipboardService.GetTextAsync) : 
-                                                                                                 Observable.Empty<string>())
-                                                                .Switch()
-                                                                .Where(clipboardText => !string.IsNullOrEmpty(clipboardText));
+            var clipboardChangedObservable = windowService.IsVisible.Select(isVisible => 
+                                                                            isVisible ? Observable.FromAsync(clipboardService.GetTextAsync) : 
+                                                                                        Observable.Empty<string>())
+                                                                    .Switch()
+                                                                    .Where(clipboardText => !string.IsNullOrEmpty(clipboardText));
 
             Text = clipboardChangedObservable.ToReactiveProperty();
 
