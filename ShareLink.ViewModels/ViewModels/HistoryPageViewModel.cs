b3d5574c1ac2f1afccc8ea4ba09fa6e11e975428
Tomasz Polanski <tomasz.polanski@futurice.com>
2014-10-24 16:52:37 +0200

    Implementing resharing

--- ShareLink.ViewModels/ViewModels/HistoryPageViewModel.cs
+++ ShareLink.ViewModels/ViewModels/HistoryPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
 using Microsoft.Practices.Prism.Mvvm.Interfaces;
+using Services.Interfaces;
 using ShareLink.Models;
 using ShareLink.Services.Interfaces;
 
@@ -16,21 +17,26 @@ namespace ShareLink.ViewModels.ViewModels
         public ObservableCollection<ShareData> ShareDataList { get; private set; }
 
         public ICommand GoBackCommand { get; private set; }
+        public ICommand ReshareCommand { get; private set; }
 
 
-
-        public HistoryPageViewModel(IShareDataRepository shareDataRepository, INavigationService navigationService)
+        public HistoryPageViewModel(IShareDataRepository shareDataRepository, INavigationService navigationService, IDataTransferService dataTransferService)
         {
             ShareDataList = new ObservableCollection<ShareData>();
 
             _sharedItemsSubscription = shareDataRepository.ShareDataObservable.Subscribe(ShareDataList.Add);
 
             GoBackCommand = new DelegateCommand(navigationService.GoBack);
+            ReshareCommand = new DelegateCommand(() => ShareLink(dataTransferService, "test", new Uri("http://www.test.test")));
         }
 
         public void Dispose()
         {
             _sharedItemsSubscription.Dispose();
+        }
+        private static void ShareLink(IDataTransferService transferService, string title, Uri uri)
+        {
+            transferService.Share(title, uri.ToString(), uri);
         }
     }
 }
