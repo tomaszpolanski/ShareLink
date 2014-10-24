3aa5826bf241a1b82d6d2b05ceae3cb835d0c619
Tomasz Polanski <tomasz.polanski@futurice.com>
2014-10-24 09:38:23 +0200

    Adding tests

--- ShareLink.Tests/Services/TestShareDataRepository.cs
+++ ShareLink.Tests/Services/TestShareDataRepository.cs
@@ -40,7 +40,7 @@ namespace ShareLink.Tests.Services
                 () => repository.ShareDataObservable, 0, 10, 100
                 );
 
-            IList<Recorded<Notification<ShareData>>> expected = new List<Recorded<Notification<ShareData>>>();
+            var expected = new List<Recorded<Notification<ShareData>>>();
 
             ReactiveAssert.AreElementsEqual(expected, actual.Messages);
         }
@@ -51,7 +51,7 @@ namespace ShareLink.Tests.Services
             var scheduler = new TestScheduler();
             var shareData = new ShareData();
             A.CallTo(() => _cacheService.GetDataAsync<ICollection<ShareData>>(A<string>.Ignored, A<CancellationToken>.Ignored))
-                .Returns(Task.FromResult<ICollection<ShareData>>(new Collection<ShareData>(){shareData}));
+                .Returns(Task.FromResult<ICollection<ShareData>>(new Collection<ShareData>{shareData}));
             var repository = CreateService();
 
             var actual = scheduler.Start(
@@ -71,7 +71,7 @@ namespace ShareLink.Tests.Services
             var shareData2 = new ShareData();
             var shareData3 = new ShareData();
             A.CallTo(() => _cacheService.GetDataAsync<ICollection<ShareData>>(A<string>.Ignored, A<CancellationToken>.Ignored))
-                .Returns(Task.FromResult<ICollection<ShareData>>(new Collection<ShareData>() { shareData1, shareData2, shareData3 }));
+                .Returns(Task.FromResult<ICollection<ShareData>>(new Collection<ShareData> { shareData1, shareData2, shareData3 }));
             var repository = CreateService();
 
             var actual = scheduler.Start(
@@ -83,6 +83,58 @@ namespace ShareLink.Tests.Services
                 OnNext(10, shareData1),
                 OnNext(10, shareData2),
                 OnNext(10, shareData3)
+            };
+
+            ReactiveAssert.AreElementsEqual(expected, actual.Messages);
+        }
+
+        [TestMethod]
+        public void MultipleSubscriptionsRetriggerSharedData()
+        {
+            var scheduler = new TestScheduler();
+            var shareData1 = new ShareData();
+            var shareData2 = new ShareData();
+            var shareData3 = new ShareData();
+            A.CallTo(() => _cacheService.GetDataAsync<ICollection<ShareData>>(A<string>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(Task.FromResult<ICollection<ShareData>>(new Collection<ShareData> { shareData1, shareData2, shareData3 }));
+            var repository = CreateService();
+
+            scheduler.Start(
+                () => repository.ShareDataObservable, 0, 10, 1000
+                );
+            var actual2 = scheduler.Start(
+                () => repository.ShareDataObservable, 1000, 1010, 2000
+                );
+
+            var expected = new[]
+            {
+                OnNext(1010, shareData1),
+                OnNext(1010, shareData2),
+                OnNext(1010, shareData3)
+            };
+
+            ReactiveAssert.AreElementsEqual(expected, actual2.Messages);
+        }
+
+        [TestMethod]
+        public void AddingNewDataUpdatesStream()
+        {
+            var scheduler = new TestScheduler();
+            var shareData1 = new ShareData();
+            var shareData2 = new ShareData();
+            A.CallTo(() => _cacheService.GetDataAsync<ICollection<ShareData>>(A<string>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(Task.FromResult<ICollection<ShareData>>(new Collection<ShareData> { shareData1}));
+            var repository = CreateService();
+
+            repository.Add(shareData2);
+            var actual = scheduler.Start(
+                () => repository.ShareDataObservable, 0, 10, 1000
+                );
+
+            var expected = new[]
+            {
+                OnNext(10, shareData1),
+                OnNext(10, shareData2)
             };
 
             ReactiveAssert.AreElementsEqual(expected, actual.Messages);
