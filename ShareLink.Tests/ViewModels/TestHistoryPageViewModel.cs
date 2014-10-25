96bcf27393dc55ac1b35f14790a87b007b48d83a
tomaszpolanski <krigerek@gmail.com>
2014-10-25 12:40:00 +0200

    Adding tests

---
+++ ShareLink.Tests/ViewModels/TestHistoryPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShareLink.ViewModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareLink.Tests.ViewModels
+{
+    [TestClass]
+    public class TestHistoryPageViewModel
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+    }
+}
