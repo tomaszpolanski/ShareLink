fa78479bed861fc919c3224aa8b4071c7945bbde
Tomasz Polanski <tomasz.polanski@futurice.com>
2014-11-20 10:09:49 +0100

    Cleanup Adding Fxcop

--- ShareLink.Services/HttpClient.cs
+++ ShareLink.Services/HttpClient.cs
@@ -1,8 +1,5 @@
 ﻿using ShareLink.Services.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ShareLink.Services
